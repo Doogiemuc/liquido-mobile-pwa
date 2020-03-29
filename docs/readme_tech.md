cabd932e5137f725c9d0aeb36b20a53d99e6756a
DoogieMuc <github@doogie.de>
2020-03-29 21:43:42 +0200

    Working on chat on start page

---
+++ docs/readme_tech.md
@@ -0,0 +1,43 @@
+# Liquido Mobile
+
+Designed for small groups, e.g. social societeies, SMEs, pupils (suggest age >10), open source communities
+Liquido Mobile has a uch simpler concept than the larger web based www.Liquido.vote
+
+## Use Case Flow
+
+ 0. Open www.liquido.me -> First user automatically becomes Admin
+ 1. Create a new poll
+ 2. Invite users (e.g. pupils, members, people who are allowed to vote) by sharing a link.
+ 3. Every user can add one(!) proposal to that poll
+ 4. Admin, eg. the teacher, starts the voting phase
+ 5. User's cast their vote
+ 6. Admin closes the voting phase
+ 7. Everyone can see the results.
+
+## Tennant
+
+One tennant contains
+
+ * one admin user
+ * a list of users (that are allowed to vote)
+ * a list of polls
+ * proposals (new, elaboration, in voting)
+ * agreed laws
+
+
+
+## Screenflow
+
+On first app start: Chat like interface.
+
+ * Hello, my name is Liquido. What's your name?
+ * Hey <name>! Nice to meet you. What topic to you want to vote on? 
+ * Ok. Now invite your friends so that they can join the poll.   <link> <QR-code>
+ * <button>Go to poll
+
+On next app starts
+
+ * Hello <name> Welcome back to Liquido.
+ * <userhome>
+ * Bar at the bottom: proposals - polls - search
+
---
+++ src/views/cast-vote.vue
@@ -0,0 +1,126 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <p>
+        Now sort these proposals into your personally preferred order. With your favorite proposal at the top.
+      </p>
+
+      <div id="ballot" class="ballot">
+        <draggable v-model="laws" :swap-threshold="0.5" group="ballot">
+          <law-panel
+            v-for="law in laws"
+            :law="law"
+            :read-only="false"
+            :key="law.id"
+          />
+        </draggable>
+      </div>
+    </div>
+    <b-button
+      type="is-primary"
+      icon-pack="fas"
+      icon-right="angle-double-right"
+      class="is-pulled-right"
+    >
+      Next
+    </b-button>
+  </section>
+</template>
+
+<script>
+import lawPanel from "../components/law-panel"
+//import Sortable from 'sortablejs'
+import draggable from "vuedraggable"
+
+export default {
+  name: "Index",
+  components: { lawPanel, draggable },
+  mixins: [],
+  data() {
+    return {
+      laws: [
+        {
+          id: 1,
+          title:
+            "Just some example very long title gasdkfljasdjf lkdjsa lkf flknwe rtmnwre,m nwm,e rwerfln werflkwe rln welrnlwlerk ",
+          description: "Law description which is very helpfull ...",
+          createdAt: new Date(),
+          createdBy: {
+            email: "user@liquido.vote",
+            profile: {
+              name: "John Doe",
+              picture: "/img/avatars/Avatar1.png",
+            },
+          },
+          numSupporters: 23,
+        },
+        {
+          id: 2,
+          title:
+            "Just some example very long title 2 222 asd fag ewtrevcvderv we ew",
+          description: "Law description which is very helpfull ...",
+          createdAt: new Date(),
+          createdBy: {
+            email: "user@liquido.vote",
+            profile: {
+              name: "Able Dune",
+              picture: "/img/avatars/Avatar3.png",
+            },
+          },
+          numSupporters: 14,
+          supportedByCurrentUser: true,
+        },
+        {
+          id: 3,
+          title: "Just some example very long 333333 ",
+          description: "Law description which is very helpfull ...",
+          createdAt: new Date(),
+          createdBy: {
+            email: "user@liquido.vote",
+            profile: {
+              name: "Simon Donahugh",
+              picture: "/img/avatars/Avatar2.png",
+            },
+          },
+          numSupporters: 12,
+          supportedByCurrentUser: true,
+        },
+      ],
+    }
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {
+    let el = document.getElementById("ballot")
+    console.log(el)
+    /*
+		new Sortable(el, {
+				animation: 150,
+				//ghostClass: 'blue-background-class'
+		});
+		*/
+  },
+  computed: {},
+  methods: {},
+  filters: {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+}
+</script>
+
+<style lang="scss">
+.ballot {
+  padding: 1rem 0.5rem;
+  margin: 0.5rem -0.5rem 1em -0.5rem;
+  background: #f0f0f0;
+}
+.law-panel {
+  cursor: grab;
+  //margin-bottom: 0.5em;
+}
+.sortable-ghost {
+  opacity: 0.2;
+}
+</style>
