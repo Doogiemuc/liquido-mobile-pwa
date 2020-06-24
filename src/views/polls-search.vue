3567c3d6a635e5c0f6dea1b4f088d72d2da9482e
DoogieMuc <github@doogie.de>
2020-06-24 11:00:44 +0200

    Responsive nav arrows

--- src/views/polls-search.vue
+++ src/views/polls-search.vue
@@ -3,7 +3,7 @@
 		<div class="liquido-header shadow-sm">
 			<div class="liquido-brand"><i class="fas fa-university"></i>&nbsp;<span class="liquido"></span></div>
 
-			<ul id="navArrows" class="nav nav-arrows" >
+			<ul id="navArrows" class="nav nav-arrows container-fluid" >
 				<li><router-link active-class="active" to="/ideas" id="IdeasArrow">	<i class="far fa-comments"></i>&nbsp;Ideas</router-link></li>
 				<li><router-link active-class="active" to="/proposals" id="ProposalsArrow"><i class="far fa-comments"></i>&nbsp;Proposals</router-link></li>
 				<li class="active"><router-link active-class="active" to="/polls" id="PollsArrow"><i class="fas fa-poll"></i>&nbsp;Polls</router-link></li>
@@ -54,24 +54,29 @@
 					<i class="fas fa-poll"></i>&nbsp;{{poll.title}}
 					<i class="float-right fas fa-angle-double-right goto-poll-icon"></i>
 				</template>
-				<div class="poll-proposal" v-for="prop in poll.proposals" :key="prop.id">
-					<div class="d-flex">
-						<img :src="'https://picsum.photos/seed/'+prop.id+'/100'" alt="proposal image" class="law-image"/>
-						<div class="d-flex flex-column w-100 justify-content-between">
-							<div class="law-title">{{prop.title}}</div>
-							<div class="d-flex justify-content-between subtitle">
-								<div>
-									<i class="far fa-user"></i>&nbsp;{{ prop.createdBy.profile.name }}
-								</div>
-								<div>
-									<i class="far fa-clock"></i>&nbsp;today
-								</div>
-								<div :class="{ supported: prop.supportedByCurrentUser }">
-									<i :class="{'far': !prop.supportedByCurrentUser, 'fas': prop.supportedByCurrentUser}" class="fa-thumbs-up"></i>&nbsp;{{prop.numSupporters}}
-								</div>
-							</div>
+				<div class="law-panel" v-for="law in poll.proposals" :key="law.id">
+					<div>
+						<h4 class="law-title"><i :class="getIconForLaw(law)" class="title-icon"></i>&nbsp;{{law.title}}</h4>
+					</div>
+					<div class="law-subtitle d-flex">
+						<div class="createdAt flex-fixed-width">
+							<i class="far fa-clock"></i>&nbsp;{{ formatDate(law.createdAt) }}
 						</div>
-					</div>	
+						<div class="user">
+							<i class="far fa-user"></i>&nbsp;{{ law.createdBy.profile.name }}
+						</div>
+						<div class="like-button flex-grow-1 text-right" :class="{ supported: law.supportedByCurrentUser }">
+							<i :class="{'far': !law.supportedByCurrentUser, 'fas': law.supportedByCurrentUser}" class="fa-thumbs-up"></i>&nbsp;{{law.numSupporters}}
+						</div>
+					</div>
+					<div class="d-flex">
+						<div class="law-image flex-fixed-width">
+							<img :src="'https://picsum.photos/seed/'+law.id+'/100'" alt="Image" class="law-image"/>
+						</div>
+						<div class="law-description">
+							{{law.description}}
+						</div>
+					</div> 
 				</div>
 			</b-card>
 		</div>
@@ -80,7 +85,7 @@
 
 <script>
 
-// import moment from 'moment'
+import moment from 'moment'
 // import pollPanel from "../components/poll-panel"
 
 export default {
@@ -99,7 +104,7 @@ export default {
 			}
 		}
 	},
-	//components: { pollPanel },
+	components: { moment },
 	data() {
 		return {
 			poll: {
@@ -131,7 +136,7 @@ export default {
 								id: 7001,
 								email: "user1@liqudo.vote",
 								profile: {
-									name: "User1 Mobile",
+									name: "User1longname Mobileasdf",
 									mobilephone: "#491234517",
 									picture: "/img/avatars/Avatar1.png",
 								}
@@ -213,6 +218,37 @@ export default {
 			return result;
 		},
 
+		getFromNow(dateVal) {
+			return moment(dateVal).fromNow()
+		},
+
+		formatDate(dateVal) {
+			return moment(dateVal).format("L")
+		},
+
+		getIconForLaw(law) {
+			switch (law.status) {
+				case "IDEA":
+					return "far fa-lightbulb"
+				case "PROPOSAL":
+					return "far fa-file-alt"
+				case "ELABORATION":
+					return "far fa-comments"
+				case "VOTING":
+					return "fas fa-vote-yea"
+				case "LAW":
+					return "fas fa-balance-scale-left"
+				case "DROPPED":
+					return "far fa-window-close"
+				case "RETENTION":
+					return "fas fa-temperature-low"
+				case "RETRACTED":
+					return "fas fa-backspace"
+				default:
+					return "fas fa-university"
+			}
+		},
+
 		transitionHeader() {
 			var navHeight = 130;  // pixel
 			if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -271,22 +307,31 @@ $navHeight: 130px;
 $inactiveNavArrowBg: #fdfdff;
 
 #navArrows {
+	flex-wrap: nowrap;
 	margin: 0 auto;
 	justify-content: center;
-	font-family: Georgia, 'Times New Roman', Times, serif;
+	font-family: 'Libre Baskerville', serif;
+	//font-family: Georgia, 'Times New Roman', Times, serif;
 	margin-bottom: 1rem;
+	padding-left: 15px;  // same as contianer-fluid. Was overwritten by .nav
+	//min-width: 350px;	
 	transition: 0.3s;
+	@media (max-width: 350px) {
+		font-size: 12px;
+	}
 }
+
 #navArrows > li {
 	margin: 0 12px;
 	position: relative;
+	flex-grow: 1;
 	transition: 0.3s;
 }
 #navArrows a {
 	display: block;
 	color: $primary;
 	height: 40px;
-	padding: 9px 3px 0 7px;
+	line-height: 40px;   // vertically center text in arrows
 	background-color: $inactiveNavArrowBg;
 	transition: 0.3s;
 }
@@ -322,7 +367,7 @@ $inactiveNavArrowBg: #fdfdff;
 	margin-bottom: 5px;
 	a {
 		height: 30px;
-		padding: 4px 4px 0 7px;
+		line-height: 30px;
 	}
 	li {
 		margin: 0 10px;
@@ -398,7 +443,8 @@ $avatar_size: 70px;
 	}
 	.law-title {
 		//TODO: break law-title after three lines:   https://codepen.io/romanrudenko/pen/ymHFh
-		line-height: 1.2;
+		//line-height: 1.2;
+		//height: 20px;
 	}
 	.law-image {
 		float: left;
@@ -411,10 +457,10 @@ $avatar_size: 70px;
 		height: $avatar_size;
 		margin-right: 10px;
 	}
-	.subtitle {
+	.law-subtitle {
 		font-size: 10px;
 		color: #BBB;
-		//line-height: 10px;
+		//line-height: 18px;
 	}
 	.supported {
 		color: green;
@@ -422,6 +468,60 @@ $avatar_size: 70px;
 	.goto-poll-icon {
 		color: $primary;
 		line-height: 1.5;   // was overwritten  by  "fas"
+	}
+}
+
+.law-panel {
+	//height: 30px + 25px + $avatar_size + 15px;  // title + subtitle + avatar_img + padding
+	//overflow: hidden;
+	padding: 10px 10px 10px 10px;
+
+	.flex-fixed-width {
+		flex: 0 0 $avatar_size + 10px;
+		//border: 1px solid red;
+	}
+
+	.law-title {
+		margin-bottom: 3px;
+		padding: 0;
+		font-size: 18px;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+
+	.title-icon {
+		font-size: 80%;
+	}
+
+	.law-subtitle {
+		font-size: 10px;
+		//line-height: 15px;
+		//height: 18px;
+		//min-height: 18px;
+		color: #BBB;
+		margin-bottom: 5px;
+		//border-bottom: 1px solid rgba(0, 0, 0, 0.1);;
+		//-webkit-box-shadow: 0 0.1rem 0.2rem rgba(0, 0, 0, 0.1);
+		//box-shadow: 0 0.1rem 0.2rem rgba(0, 0, 0, 0.1);
+	}
+
+	.law-image {
+		//margin: 5px;
+		border-radius: 5px;
+		min-width: $avatar_size;
+		max-width: $avatar_size;
+		width: $avatar_size;
+		min-height: $avatar_size;
+		max-height: $avatar_size;
+		height: $avatar_size;
+	}
+
+	.law-description {
+		font-size: 12px;
+		height: $avatar_size;
+		max-height: $avatar_size;
+		overflow: hidden;
 	}
 }
 
